08fff8be5992ac4d4f2c9e7f51e51e1f9fc04cb9
Pavan Uthsara <uthsarapavan@gmail.com>
2025-12-22 22:07:13 +0530

    Implement BaseEntity and TicketEnums

---
+++ src/TaskFlow.Domain/Common/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ src/TaskFlow.Domain/Enums/TicketEnums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Enums
+{
+    public enum PriorityLevel
+    { 
+        Low = 0,
+        Medium = 1,
+        High = 2,
+        Critical = 3
+    }
+
+    public enum TicketStatus
+    { 
+        Todo = 0,
+        InProgress = 1,
+        InReview = 2,
+        Done = 3,
+    }
+}
