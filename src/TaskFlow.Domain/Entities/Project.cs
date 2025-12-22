2ed7c64e8f35da57a943da0d36941a7dd8ea28d2
Pavan Uthsara <uthsarapavan@gmail.com>
2025-12-22 22:21:37 +0530

    Add Project and Ticket entity classes

---
+++ src/TaskFlow.Domain/Entities/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Common;
+
+namespace TaskFlow.Domain.Entities
+{
+    public class Project : BaseEntity
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        // One-to-Many relationship: One project has many tickets
+        public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
---
+++ src/TaskFlow.Domain/Entities/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Common;
+using TaskFlow.Domain.Enums;
+
+namespace TaskFlow.Domain.Entities
+{
+    public class Ticket : BaseEntity
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(2000)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime? DueDate { get; set; }
+        public PriorityLevel Priority { get; set; } = PriorityLevel.Medium;
+        public TicketStatus Status { get; set; } = TicketStatus.Todo;
+
+        // 1. Relationship to Project (Many to One)
+        // A ticket must belong to a project
+        public int ProjectId { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual Project Project { get; set; };
+
+    }
+}
